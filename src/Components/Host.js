9f8a0fabf36c19a346f168a860001d5ea81c7391
Vishnu Swaroop <97403550+Vswaroop04@users.noreply.github.com>
2023-02-11 21:50:13 +0530

    Update Host.js

--- src/Components/Host.js
+++ src/Components/Host.js
@@ -1,5 +1,5 @@
 const DATA = {
-  URL: "https://sudo-delete-web-service-tech-fest-backend.onrender.com"
+  URL: "https://iiit.centralindia.cloudapp.azure.com/"
   // URL: "http://horizon-iiitvicd.in:3001"
 };
 
